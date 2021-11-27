d274f84ca88a0962c04740517194e35ff2ff2a65
shinyejin0212 <shinyejin0212@gmail.com>
2021-11-27 22:13:30 +0900

    ver3

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,4 +6,5 @@ app_name="recipe"
 urlpatterns = [
     path("matching/<int:id>",matching, name="matching" ),
     path("db_recipe/",db_recipe, name="db_recipe"),
-] 
+    path("recipe_all/",recipe_all, name="recipe_all"),
+]
